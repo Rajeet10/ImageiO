0d2e300e3286e25b5a63657ae63f9ca929485a99
Rajeet <rajeetdps@gmail.com>
2021-07-10 15:01:44 +0530

    React hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,86 @@
-import React,{Component} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import './App.css';
 import Images from './components/Images';
 
 
-class App extends Component{
-  constructor(props){
-    console.log('App constructor');
-    super(props);
-    this.state={title:"hello",isShowing:false};
-  }
-  componentDidMount(){
-    console.log("APP MOUNTED");
-    // this.setState({title:"HI"});
-  }
-  componentDidUpdate(){
-    console.log("app updated")
-}
+// class App extends Component{
+//   constructor(props){
+//     console.log('App constructor');
+//     super(props);
+//     this.state={title:"hello",isShowing:false};
+//   }
+//   componentDidMount(){
+//     console.log("APP MOUNTED");
+//     // this.setState({title:"HI"});
+//   }
+//   componentDidUpdate(){
+//     console.log("app updated")
+// }
 
-  handleclick=()=>{
-    this.setState({isShowing:!this.state.isShowing});
-  }
+//   handleclick=()=>{
+//     this.setState({isShowing:!this.state.isShowing});
+//   }
 
-  render(){
-    console.log('App Render');
-    return(
+//   render(){
+//     console.log('App Render');
+//     return(
+//       <section className="flex justify-center">
+//         <div className="w-1/2">
+//           <div className="text-center">
+//         <div className="my-4">{this.state.title}</div>
+//         <button 
+//         className="p-1 bg-blue-700 text-white my-2"
+//         onClick={this.handleclick}
+//         >Toggle Image</button>
+//         </div>
+//         {
+//           this.state.isShowing?
+//          (<Images/>):
+//          null
+//         }
+        
+//         </div>
+//       </section>
+      
+//     )
+//   }
+// }
+
+function App(){
+  const [title, setTitle] = useState("hello");
+  const[isShowing,setIsShowing]=useState(false);
+  const [didMount, setdidMount] = useState(false);
+  const mountRef=useRef(false); 
+  useEffect(()=>{//component did mount only
+    setdidMount(true);
+    console.log("app mounted");
+  },[])
+
+  useEffect(()=>{//component will update
+    if(mountRef.current){
+      console.log("app updated");
+    }else{
+      mountRef.current=true;
+    }
+    
+  },[isShowing])
+
+  const handleclick=()=>{
+   setIsShowing(!isShowing);
+  }
+      return(
       <section className="flex justify-center">
+        {console.log("rerendered")}
         <div className="w-1/2">
           <div className="text-center">
-        <div className="my-4">{this.state.title}</div>
+        <div className="my-4">{title}</div>
         <button 
         className="p-1 bg-blue-700 text-white my-2"
-        onClick={this.handleclick}
+        onClick={handleclick}
         >Toggle Image</button>
         </div>
         {
-          this.state.isShowing?
+          isShowing?
          (<Images/>):
          null
         }
@@ -43,7 +89,6 @@ class App extends Component{
       </section>
       
     )
-  }
-}
 
+}
 export default App;
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Images extends Component {
-    constructor(props){
-        super(props);
-        this.state={interval:null};
+// export default class Images extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state={interval:null};
 
-    }
-    componentDidMount(){
-        console.log("image comp mounted");
-        this.setState({
-            interval:setInterval(() => {
-                console.log("hello");
-            }, 1000)
-        })
-    }
-    componentWillUnmount(){
-        console.log("Images comp unmounted");
-        clearInterval(this.state.interval) ;
-      }
+//     }
+//     componentDidMount(){
+//         console.log("image comp mounted");
+//         this.setState({
+//             interval:setInterval(() => {
+//                 console.log("hello");
+//             }, 1000)
+//         })
+//     }
+//     componentWillUnmount(){
+//         console.log("Images comp unmounted");
+//         clearInterval(this.state.interval) ;
+//       }
 
   
-    render() {
-        return (
-            <img src="https://images.unsplash.com/photo-1622495966027-e0173192c728?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
-        )
-    }
+//     render() {
+//         return (
+//             <img src="https://images.unsplash.com/photo-1622495966027-e0173192c728?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
+//         )
+//     }
+// }
+
+import React,{useEffect,useState} from 'react'
+
+export default function Images() {
+    // const [myinterval, setMyInterval] = useState(null);
+    useEffect(()=>{
+        console.log("image mounted");
+      const interval=  setInterval(()=>{
+            console.log("hello");
+        },1000)
+        
+        return()=>{
+            console.log("image unmounted");
+            clearInterval(interval);
+        }
+    },[])
+
+ 
+    return (
+        <img src="https://images.unsplash.com/photo-1622495966027-e0173192c728?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
+    )
 }
