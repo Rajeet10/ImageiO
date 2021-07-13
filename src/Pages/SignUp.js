f2b7af8b8ae01b31f84997472374eaccd5909141
Rajeet <rajeetdps@gmail.com>
2021-07-13 20:37:57 +0530

    using formic api

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,46 +1,24 @@
 import React,{useState} from 'react';
-import { Redirect } from 'react-router-dom';
-import firebase from '../config/Firebase';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const SignUp = () => {
-
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState("");
-    const [form, setForm] = useState({email:"",password:""});
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    const handleForm=(e)=>{
-        if (isLoading) return ;
-        setIsLoading(true);
-        e.preventDefault();
-        firebase
-        .auth()
-        .signInWithEmailAndPassword(form.email,form.password)
-        .then((res)=>{
-            setIsLoggedIn(true);
-            setError("");
-            setIsLoading(false);
-        }).catch(e=>{
-           setError(e.message);
-            setIsLoading(false);
-        });
-    };
-
-    const handleInput=(e)=>{
-        setForm({
-            ...form,
-           [ e.target.name]:e.target.value
-
-        })
-    }
-
-    if(isLoggedIn) return <Redirect  to="/" exact/>
+    const formik=useFormik({
+        initialValues:{email:"",password:""},
+        onSubmit:value=>{
+            console.log("Formik",value)
+        },
+      validationSchema:Yup.object({
+          email:Yup.string().required('Email is Required').email('Email is invalid'),
+          password:Yup.string().required("Password  is required").min(6),
+      }),
+        
+    });
     return (
         <div className="flex h-screen bg-gray-200">
         <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-purple-800 to-purple-600">
 
-          <form className="m-5 w-10/12" onSubmit={handleForm}>
-              {(error !== "") && <p>{error}</p>}
+          <form className="m-5 w-10/12" onSubmit={formik.handleSubmit}>
           <h1 className="w-full text-4xl tracking-widest text-center my-6">
             SignUp
           </h1> 
@@ -49,30 +27,42 @@ const SignUp = () => {
                   className="p-2 rounded shadow w-full text-black"
                   placeholder="Email Id"
                   name="email"
-                  value={form.email}
-                  onChange={handleInput}
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   />
+                  {
+                      formik.touched.email && formik.errors.email ?
+                        <p>{formik.errors.email}</p>:null
+                  }
+                  
               </div>
               <div className="w-full my-6">
                   <input type="password" 
                    className="p-2 rounded shadow w-full text-black"
                    placeholder="Password"
                    name="password"
-                   value={form.password}
-                   onChange={handleInput}
+                   value={formik.values.password}
+                   onChange={formik.handleChange}
+                   onBlur={formik.handleBlur}
                    />
+                   {
+                      formik.touched.password && formik.errors.password ?
+                        <p>{formik.errors.password}</p>:null
+                  }
               </div>
               <div className="w-full my-10">
                   <button type="submit"  
                   className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-700 to-yellow-400 text-black" 
-                  >{
+                  >
+                      {/* {
                       isLoading ? (
                       <i className="fas fa-circle-notch fa-spin"></i>
                       ) :
-                      (
-                          "Login"
-                      )
-                  }
+                      ( */}
+                          SignUp
+                      {/* )
+                  } */}
                   </button>
               </div>
           </form>
