8a50f2a09f2e124f06fa82748b7d3d9e9eb63214
Rajeet <rajeetdps@gmail.com>
2021-07-13 22:27:16 +0530

    Removing underline in link

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,12 +23,16 @@ const Header = () => {
              <nav className="py-5 bg-gray-900 text-white flex justify-between">
           <ul className="flex justify-between px-10">
             <li className="mr-5">
-              <NavLink to="/"  exact activeClassName="underline text-yellow-200">
+              <NavLink to="/"  exact activeClassName="underline text-yellow-200"
+              style={{textDecoration:'none'}}
+              >
                 Home
                 </NavLink>
             </li>
             <li className="mr-5">
-            <NavLink to="/gallery" activeClassName="underline text-yellow-200">
+            <NavLink to="/gallery" activeClassName="underline text-yellow-200"
+            style={{textDecoration:'none'}}
+            >
               Gallery
               </NavLink>
             </li>
@@ -40,7 +44,9 @@ const Header = () => {
                 <button onClick={logout}>Logout</button>
               ) :
               (
-                <NavLink to="/login" activeClassName="underline text-yellow-200">
+                <NavLink to="/login" activeClassName="underline text-yellow-200"
+                style={{textDecoration:'none'}}
+                >
                   Login
                   </NavLink>
               )
