2a16a9d165df35ebccd0e51f4548b4358bdf4daa
Rajeet <rajeetdps@gmail.com>
2021-07-13 20:50:23 +0530

    Modifying code with formik

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,49 +1,43 @@
 import React,{useState} from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
 import * as Yup from 'yup';
 
 const SignUp = () => {
-    const formik=useFormik({
-        initialValues:{email:"",password:""},
-        onSubmit:value=>{
+    return (
+        <Formik 
+        initialValues={{email:"",password:""}}
+        onSubmit={(value)=>{
             console.log("Formik",value)
-        },
-      validationSchema:Yup.object({
+        }}
+      validationSchema={Yup.object({
           email:Yup.string().required('Email is Required').email('Email is invalid'),
           password:Yup.string().required("Password  is required").min(6),
-      }),
-        
-    });
-    return (
+      })}
+        >
         <div className="flex h-screen bg-gray-200">
         <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-purple-800 to-purple-600">
 
-          <form className="m-5 w-10/12" onSubmit={formik.handleSubmit}>
+          <Form className="m-5 w-10/12" >
           <h1 className="w-full text-4xl tracking-widest text-center my-6">
             SignUp
           </h1> 
               <div className="w-full my-6 ">
-                  <input type="email" 
+                  <Field 
+                  name="email"
+                  type="email" 
                   className="p-2 rounded shadow w-full text-black"
                   placeholder="Email Id"
-                  {...formik.getFieldProps("email")}
                   />
-                  {
-                      formik.touched.email && formik.errors.email ?
-                        <p>{formik.errors.email}</p>:null
-                  }
-                  
+                   <ErrorMessage name="email" />
               </div>
               <div className="w-full my-6">
-                  <input type="password" 
-                   className="p-2 rounded shadow w-full text-black"
-                   placeholder="Password"
-                  {...formik.getFieldProps("password")}
-                   />
-                   {
-                      formik.touched.password && formik.errors.password ?
-                        <p>{formik.errors.password}</p>:null
-                  }
+                  <Field
+                     name="password"
+                     type="password" 
+                     className="p-2 rounded shadow w-full text-black"
+                     placeholder="Password"
+                  />
+                   <ErrorMessage name="password" />
               </div>
               <div className="w-full my-10">
                   <button type="submit"  
@@ -59,9 +53,10 @@ const SignUp = () => {
                   } */}
                   </button>
               </div>
-          </form>
+          </Form>
           </div>
       </div>
+      </Formik>
       );
     }
 
